4988bff9c0b7d746462a00b817b175a8ff530c40
hacadena28 <hacadena@unicesar.edu.cong>
2022-11-19 23:35:00 -0500

    61

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    
-
-  ],
-  imports: [
-    BrowserModule,
-    HeroesModule,
-    ContadorModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HeroesModule,
+        ContadorModule,
+        DbzModule
+    ]
 })
 export class AppModule { }
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent {
+
+  personajes: any=[]
+}
